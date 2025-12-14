3b59140a3b7a94b9cf48a3b209ac0c50fa3ea26b
SAUMYA-DEVELOPER <saumyaaggarwalji@gmail.com>
2025-12-14 14:47:34 +0530

    Update renderer.js

--- public/src/core/renderer.js
+++ public/src/core/renderer.js
@@ -1,12 +1,10 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 
 export function initRenderer() {
-  const r = new THREE.WebGLRenderer({
-    antialias: true,
-    powerPreference: "high-performance"
-  });
+  const r = new THREE.WebGLRenderer({ antialias: false });
   r.setSize(window.innerWidth, window.innerHeight);
-  r.setPixelRatio(Math.min(devicePixelRatio, 2));
-  r.setClearColor(0x000010, 1);
+  r.setPixelRatio(window.devicePixelRatio);
+  r.setClearColor(0x000000, 1);
+  document.body.appendChild(r.domElement);
   return r;
 }
