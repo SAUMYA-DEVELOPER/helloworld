3c4434bdb64be4ca8a185c1830fb3aa99eb5954d
SAUMYA-DEVELOPER <saumyaaggarwalji@gmail.com>
2025-12-14 15:11:19 +0530

    Delete public directory

--- public/src/core/resize.js
+++
@@ -1,5 +0,0 @@
-export function handleResize(renderer, camera) {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
